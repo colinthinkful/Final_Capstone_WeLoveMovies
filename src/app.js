e01bb6aa70628ab7cb4e43898b7738649b542aa5
Colin Harrison <colinharrison@indeed.com>
2024-02-15 15:37:52 -0800

    Enable CORS for entire app

--- src/app.js
+++ src/app.js
@@ -5,9 +5,11 @@ const app = express();
 const moviesRouter = require("./movies/movies.router");
 const theatersRouter = require("./theaters/theaters.router");
 const reviewsRouter = require("./reviews/reviews.router");
+const cors = require("cors");
 
 // Routing
 app.use(express.json());
+app.use(cors());
 app.use("/movies", moviesRouter);
 app.use("/theaters", theatersRouter);
 app.use("/reviews", reviewsRouter);
